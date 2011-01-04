5fde1e5be5ffd2283caca49f7bbd1f028ad1f050
TWer <twer@MC375-SL-10SEP.local>
2011-01-04 15:19:55 +0800

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keepin::Application.config.secret_token = '32348823f264c32e7f78dd8e20295fe35418293e48054c02e8cd4014ecf2ac023571b83a0d129c21a36753233bb9a641c2dba7e2fa712ef7a4ad131268553243'
