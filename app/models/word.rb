98d8d5b87fff402d2946ec3b96974708d2750c01
TWer <twer@device-dc6a64.(none)>
2011-01-29 11:06:18 +1100

    refactored the word, separated the translation.

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,16 +2,16 @@ class Word < ActiveRecord::Base
   validates :word, :presence => true 
   validates :translation, :presence => true
   belongs_to :user
-  attr_accessor :translation
   
   def translate!
-    translated = Net::HTTP.get 'ajax.googleapis.com', '/ajax/services/language/translate?v=1.0&q='+ hash[:word] + '&langpair=en|zh-CN'
+    translated = Net::HTTP.get 'ajax.googleapis.com', '/ajax/services/language/translate?v=1.0&q='+ word + '&langpair=en|zh-CN'
     j = ActiveSupport::JSON
     encoded = j.decode(translated)
-    @translation = encoded["responseData"]["translatedText"]  
+    self.translation = encoded["responseData"]["translatedText"]  
   end
   
   def self.create(hash)
-      Word.new(hash)
+      word = Word.new(hash)
+      return word
   end 
 end
