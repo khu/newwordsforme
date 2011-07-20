b81cb0e96048c7da823513e42118a18676fcc7e3
lishoubo <sbli@thoughtworks.com>
2011-07-20 11:02:36 +0800

    [Zhanghuihua & Lishoubo] #7 rewrite the tags list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,46 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :except => [:show, :new, :create]
+  before_filter :authenticate, :except => [:show, :new, :create, :show_word_by_tag]
   
   def show
       tag_name = params[:name]
       @user = User.find(params[:id])
       @tabs = Tabs.new.logged_in @user
-      if tag_name
-        @words = Array.new
-        @user.word.each do |word|
-          is_has_tag = false
-          word.tags.each do |tag|
-            if tag.name == tag_name
-              is_has_tag = true
-              break
-            end
-          end
-
-          if is_has_tag
-            @words.push(word)
-          end
-
-        end
-      else
-        @words = @user.word.reverse;
-      end
+      # if tag_name
+      #   @words = Array.new
+      #   @user.word.each do |word|
+      #     is_has_tag = false
+      #     word.tags.each do |tag|
+      #       if tag.name == tag_name
+      #         is_has_tag = true
+      #         break
+      #       end
+      #     end
+      # 
+      #     if is_has_tag
+      #       @words.push(word)
+      #     end
+      # 
+      #   end
+      # else
+      #   @words = @user.word.reverse;
+      # end
+      @words = @user.word.reverse;
       @title = "Settings"
       
       @word_list = Word.find(:all, :conditions => "user_id = #{@user.id}", :order => "updated_at DESC")
   end
-
+  
+  def show_word_by_tag
+    tag_name = params[:name]
+    @user = User.find(params[:id])
+    @words = @user.word.find_all do |word|
+      if word.tags.find_by_name(tag_name)
+        word.id
+      end
+    end
+    @title = "Show words by tag"
+    @words.reverse
+  end
   
   def new
       @user = User.new
