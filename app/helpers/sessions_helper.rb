dfc48a573ffae58d1a642a46c4bbab67bc0cb0af
zhenyang <nwu.scott119@gmail.com>
2011-08-23 10:56:31 +0800

    [Yang Zhen] remove pagination and change some stylesheets

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -44,7 +44,7 @@ module SessionsHelper
   def deny_access
     store_location ## Listing 10.16
     flash[:notice] = "Please sign in to access this page."
-    redirect_to signin_path
+    redirect_to root_path
   end
 
   ## Listing 10.16
