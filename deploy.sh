54229d45b48eb4aa2a76fbb384496ce62e05f67d
Nicholas Ren <nicholas85211@gmail.com>
2011-09-07 11:11:14 +0800

    [Ren xiaojun && Li shoubo] change deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ ssh root@10.29.2.11  /bin/bash<<\EOF &
 	source /etc/profile
 	cd /home/ubuntu/ 
 	source deploy_script
-	fetch_release reaapi 10.29.2.6 /tmp/app-release keepin.tar
-	deploy keepin.tar /home/ubuntu/ keepin &
+	fetch_release reaapi 10.29.2.6 /tmp/ keepin.tar.gz
+	deploy keepin.tar.gz /home/ubuntu/ keepin &
 EOF
