92b5fc9f39003fefe3edb8fe91d7d841c8ca094e
Nicholas Ren <nicholas85211@gmail.com>
2011-09-07 10:39:16 +0800

    [Ren xiaojun & Li shoubo] make a ssh automatic deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
-cd ../keepin-release
-tar --exclude=.git -cvf  keepin.tar  keepin/
-cp  keepin.tar /etc/puppet/modules/keepin-deployment/files/keepin.tar
+ssh root@10.29.2.11  /bin/bash<<\EOF &
+	source /etc/profile
+	cd /home/ubuntu/ 
+	source deploy_script
+	fetch_release reaapi 10.29.2.6 /tmp/app-release keepin.tar
+	deploy keepin.tar /home/ubuntu/ keepin &
+EOF
