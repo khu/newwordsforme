fec119220a86984151f799086d24a8671089870f
Nicholas Ren <nicholas85211@gmail.com>
2011-09-07 14:51:15 +0800

    [Ren xiaojun] extract parameters from deploy script, caller should setup ENV varible before  calling this script

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,10 @@
 #   3.run deploy script on target node    
 
 #copy release to target node
-scp  /tmp/keepin.tar.gz root@10.29.2.11:/tmp/
+scp  /tmp/keepin.tar.gz $target_username@$target_ip:/tmp/
+
 #copy deploy script to targe node
-scp  ~/projects/keepin/deploy_script root@10.29.2.11:/home/ubuntu
+scp  ./deploy_script $target_username@$target_ip:$target_deploy_path
 
 #deploy release
-ssh root@10.29.2.11  /bin/bash<<\EOF &
-	source /etc/profile
-	cd /home/ubuntu/
-	source deploy_script
-	deploy keepin.tar.gz /home/ubuntu/ keepin
-EOF
+ssh root@10.29.2.11 "source /etc/profile && cd $target_deploy_path && source deploy_script && deploy keepin.tar.gz $target_deploy_path $target_app_name"
